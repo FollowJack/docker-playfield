c1400139bab3da2f30f8b801c84ac9775f763501
Deniel Horvatic <deniel.horvatic@porschedigitallab.com>
2018-05-24 11:37:53 +0200

    Played around with parameter

--- python/01_run_container.sh
+++ python/01_run_container.sh
@@ -1,5 +1,5 @@
-echo RUN COMMAND: docker run -it --rm --name my-running-app hello-world-python
-docker run -it --rm --name my-running-app hello-world-python
+echo RUN COMMAND: docker run -it --rm --name instance1 hello-world-python
+docker run -it --rm --name instance1 hello-world-python
 
 
 # /bin/sh at the end to open the shell otherwise it will end after the last command
